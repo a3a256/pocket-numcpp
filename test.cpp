4f1c9f9aa3b46894dba23b3749dc4d793b9750e9
a3a256 <81564718+a3a256@users.noreply.github.com>
2023-05-02 01:07:32 +0100

    tested typename@

--- test.cpp
+++ test.cpp
@@ -1,3 +1,16 @@
 #include <iostream>
 #include "numcpp/cpp_arrays.hpp"
 #include <vector>
+
+
+int main(){
+
+    NdArray<int> np;
+
+    std::vector<std::vector<int>> arr = {{1, 2, 3, 4, 5}};
+
+    np.array2d(arr);
+
+
+    return 0;
+}
