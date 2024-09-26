72b931b04e5b1610ecf7309ab98dd890238a3765
a3a256 <81564718+a3a256@users.noreply.github.com>
2024-09-26 01:01:17 +0100

    tested [] overload operators for shape

--- test.cpp
+++ test.cpp
@@ -6,6 +6,7 @@
 int main(){
 
     // testing NdArray cases
+    // yet to test on higher dimensions
 
     std::vector<std::vector<int>> a = {{1, 2}, {3, 4}};
     std::vector<float> b = {1.1f, 2.1f, 3.5f, 4.9f};
@@ -17,6 +18,14 @@ int main(){
     t = np.det(np);
 
     std::cout << t << '\n';
+
+    // tetsing [] overloading operators
+
+    NdArray ap(a);
+
+    std::cout << " Overloading operator\n";
+
+    std::cout << ap.shape[2] << '\n';
     
 
     return 0;
