c7d94d714a39e1168d5b86f4b35f7f3f3543c1f0
a3a256 <81564718+a3a256@users.noreply.github.com>
2024-10-27 18:00:09 +0000

    fixes

--- test.cpp
+++ test.cpp
@@ -5,9 +5,6 @@
 
 int main(){
 
-    // testing NdArray cases
-    // yet to test on higher dimensions
-
     std::vector<std::vector<int>> a = {{1, 2}, {3, 4}};
     std::vector<float> b = {1.1f, 2.1f, 3.5f, 4.9f};
 
